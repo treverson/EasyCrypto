3f37b7a392bb17de7d28f37701ca7e28abc0a6e7
Adam Kasperowicz <kadek750@gmail.com>
2017-12-26 12:45:49 +0100

    Periodic requests are now possible

--- module_comm/bot.py
+++ module_comm/bot.py
@@ -1,3 +1,6 @@
+from twisted.internet import reactor, task
+
+
 class Bot:
 
     def __init__(
@@ -20,13 +23,24 @@ class Bot:
         self.__protocol.do(self.__action)
 
     def action(self, data):
-
         self.__parser.process(data, self.__action, self.__parameters)
+
+        if self.__interval_defined():
+            interval = self.__get_interval()
+            task.deferLater(reactor, interval, self.__protocol.do, self.__action)
 
     def stop(self):
 
         self.done = True
         self.__protocol.stop()
+
+    def __interval_defined(self):
+
+        return "interval" in self.__parameters and len(self.__parameters["interval"]) > 0
+
+    def __get_interval(self):
+
+        return int(self.__parameters["interval"])
 
     def __str__(self):
 
